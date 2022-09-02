e8cddfae38de15e5472ab9a8c3fdcd3f403d4529
autonominas <112204995+autonominas@users.noreply.github.com>
2022-09-02 12:01:03 -0300

    adiciona recurso tema

---
+++ autonominas/autonominas/src/main/java/org/generation/autonominas/repository/TemaRepository.java
@@ -0,0 +1,17 @@
+package org.generation.autonominas.repository;
+
+import java.util.List;
+
+import org.generation.autonominas.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TemaRepository  extends  JpaRepository<Tema, Long> {
+
+public List<Tema>findAllByTipoContainingIgnoreCase(@Param("tipo")String tipo);
+	
+
+}
